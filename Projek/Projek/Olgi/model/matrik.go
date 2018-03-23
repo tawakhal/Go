142e53061f90b80f4f4ee4f5ef8ada0cf1ce2775
tawakhal <37650248+tawakhal@users.noreply.github.com>
2018-03-23 13:49:27 +0700

    Create matrik.go

---
+++ Projek/Projek/Olgi/model/matrik.go
@@ -0,0 +1,12 @@
+package model
+
+type Matrik struct {
+	NilaiBarisAtas  int
+	NilaiBarisKiri  int
+	NilaiBarisBawah int
+	NilaiBarisKanan int
+	NilaiTengah     int
+	NilaiLebar      int
+	NilaiPanjang    int
+	N               int
+}
